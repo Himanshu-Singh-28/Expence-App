576024003e42b0ba82e7d0893bafb3dec2166096
Himanshu Kumar Singh <himanshusingh1234h93@gmail.com>
2024-07-26 10:07:07 +0530

    2nd update

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import "./main.css";
 import ContextProvider from "./ContextProvider.jsx";
 
-// export const server = `https://expence-app-dev1.onrender.com/api/v2`;
-export const server = `/api/v2`;
+export const server = `https://expence-app-dev1.onrender.com/api/v2`;
+// export const server = `/api/v2`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
